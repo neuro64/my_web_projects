84dec8e9b4957b0905f313cc29571c5fac6c7a4c
neuro64 <45124615+neuro64@users.noreply.github.com>
2022-11-15 13:32:54 +0300

    Add files via upload

--- project_web_page_certificates/js/style.js
+++ project_web_page_certificates/js/style.js
@@ -10,3 +10,28 @@ document.querySelectorAll(".link").forEach(el=>{
         document.querySelector(`[data-section=${el.id}]`).classList.add("active")
     })
 })
+
+let resultOfTopNav=false;
+let resultOfMenu=false;
+document.querySelector(".hamburger").addEventListener("click",function(e)
+{
+  resultOfMenu=document.querySelector(".hamburger").classList.toggle("open")
+    resultOfTopNav= document.querySelector(".openNav").classList.toggle("open")
+     
+})
+
+
+document.querySelectorAll(".nav-linkMobile").forEach(e=>{
+  e.addEventListener("click",function(){
+    
+       resultOfTopNav = document.querySelector(".openNav").classList.toggle("open")
+       resultOfMenu=document.querySelector(".hamburger").classList.toggle("open")
+       document.querySelectorAll(".contents").forEach(el=>{
+        if(el.classList.contains("active")) 
+            el.classList.remove("active")
+        }
+    )
+    document.querySelector(`[data-section=${e.id}]`).classList.add("active")
+    
+  })
+})
